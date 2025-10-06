7241a5e3739a9162bc6fba89d16d6ef596de078e
Lukas <lukas.karlsson@chasacademy.se>
2025-10-06 20:20:40 +0200

    Add project files.

---
+++ ChessBoardNew/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ChessBoardNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
